1d61d4d82690dd79d1e7db68831b8518d2126804
JANMAY007 <jrbhatt18p10@gmail.com>
2024-04-03 12:12:07 +0530

    Changed Models and Admin

--- AashramFinance/admin.py
+++ AashramFinance/admin.py
@@ -3,11 +3,100 @@ from .models import (DandiSamarpanAashram, KutchSamarpanAashram, SaurastraSamarp
                      GujaratSamarpanAashram, GoaSamarpanAashram, MadhyaBharatSamarpanAashram,
                      RajasthanSamarpanAashram, DakshinBharatShreeShivkrupanandSwamiMath)
 
-admin.site.register(DandiSamarpanAashram)
-admin.site.register(KutchSamarpanAashram)
-admin.site.register(SaurastraSamarpanAashram)
-admin.site.register(GujaratSamarpanAashram)
-admin.site.register(GoaSamarpanAashram)
-admin.site.register(MadhyaBharatSamarpanAashram)
-admin.site.register(RajasthanSamarpanAashram)
-admin.site.register(DakshinBharatShreeShivkrupanandSwamiMath)
+
+class DandiSettings(admin.ModelAdmin):
+    list_display = ['description', 'manager', 'vyavasthapak', 'trustee', 'finance_house_head', 'Ambareeshbhai',
+                    'Cashier', 'quotation_file_1', 'quotation_file_2', 'quotation_file_3', 'bill_file',
+                    'payment_1', 'payment_1_datetime',
+                    'payment_2', 'payment_2_datetime',
+                    'payment_3', 'payment_3_datetime']
+    readonly_fields = ['manager_updated_at', 'vyavasthapak_updated_at', 'trustee_updated_at',
+                       'finance_house_head_updated_at', 'Ambareeshbhai_updated_at', 'Cashier_updated_at',
+                       'payment_1_datetime', 'payment_2_datetime', 'payment_3_datetime', 'created_at']
+
+
+class KutchSettings(admin.ModelAdmin):
+    list_display = ['description', 'manager', 'vyavasthapak', 'trustee', 'finance_house_head', 'Ambareeshbhai',
+                    'Cashier', 'quotation_file_1', 'quotation_file_2', 'quotation_file_3', 'bill_file',
+                    'payment_1', 'payment_1_datetime',
+                    'payment_2', 'payment_2_datetime',
+                    'payment_3', 'payment_3_datetime']
+    readonly_fields = ['manager_updated_at', 'vyavasthapak_updated_at', 'trustee_updated_at',
+                       'finance_house_head_updated_at', 'Ambareeshbhai_updated_at', 'Cashier_updated_at',
+                       'payment_1_datetime', 'payment_2_datetime', 'payment_3_datetime', 'created_at']
+
+
+class SaurastraSettings(admin.ModelAdmin):
+    list_display = ['description', 'manager', 'vyavasthapak', 'trustee', 'finance_house_head', 'Ambareeshbhai',
+                    'Cashier', 'quotation_file_1', 'quotation_file_2', 'quotation_file_3', 'bill_file',
+                    'payment_1', 'payment_1_datetime',
+                    'payment_2', 'payment_2_datetime',
+                    'payment_3', 'payment_3_datetime']
+    readonly_fields = ['manager_updated_at', 'vyavasthapak_updated_at', 'trustee_updated_at',
+                       'finance_house_head_updated_at', 'Ambareeshbhai_updated_at', 'Cashier_updated_at',
+                       'payment_1_datetime', 'payment_2_datetime', 'payment_3_datetime', 'created_at']
+
+
+class GujaratSettings(admin.ModelAdmin):
+    list_display = ['description', 'manager', 'vyavasthapak', 'trustee', 'finance_house_head', 'Ambareeshbhai',
+                    'Cashier', 'quotation_file_1', 'quotation_file_2', 'quotation_file_3', 'bill_file',
+                    'payment_1', 'payment_1_datetime',
+                    'payment_2', 'payment_2_datetime',
+                    'payment_3', 'payment_3_datetime']
+    readonly_fields = ['manager_updated_at', 'vyavasthapak_updated_at', 'trustee_updated_at',
+                       'finance_house_head_updated_at', 'Ambareeshbhai_updated_at', 'Cashier_updated_at',
+                       'payment_1_datetime', 'payment_2_datetime', 'payment_3_datetime', 'created_at']
+
+
+class GoaSettings(admin.ModelAdmin):
+    list_display = ['description', 'manager', 'vyavasthapak', 'trustee', 'finance_house_head', 'Ambareeshbhai',
+                    'Cashier', 'quotation_file_1', 'quotation_file_2', 'quotation_file_3', 'bill_file',
+                    'payment_1', 'payment_1_datetime',
+                    'payment_2', 'payment_2_datetime',
+                    'payment_3', 'payment_3_datetime']
+    readonly_fields = ['manager_updated_at', 'vyavasthapak_updated_at', 'trustee_updated_at',
+                       'finance_house_head_updated_at', 'Ambareeshbhai_updated_at', 'Cashier_updated_at',
+                       'payment_1_datetime', 'payment_2_datetime', 'payment_3_datetime', 'created_at']
+
+
+class MadhyaBharatSettings(admin.ModelAdmin):
+    list_display = ['description', 'manager', 'vyavasthapak', 'trustee', 'finance_house_head', 'Ambareeshbhai',
+                    'Cashier', 'quotation_file_1', 'quotation_file_2', 'quotation_file_3', 'bill_file',
+                    'payment_1', 'payment_1_datetime',
+                    'payment_2', 'payment_2_datetime',
+                    'payment_3', 'payment_3_datetime']
+    readonly_fields = ['manager_updated_at', 'vyavasthapak_updated_at', 'trustee_updated_at',
+                       'finance_house_head_updated_at', 'Ambareeshbhai_updated_at', 'Cashier_updated_at',
+                       'payment_1_datetime', 'payment_2_datetime', 'payment_3_datetime', 'created_at']
+
+
+class RajasthanSettings(admin.ModelAdmin):
+    list_display = ['description', 'manager', 'vyavasthapak', 'trustee', 'finance_house_head', 'Ambareeshbhai',
+                    'Cashier', 'quotation_file_1', 'quotation_file_2', 'quotation_file_3', 'bill_file',
+                    'payment_1', 'payment_1_datetime',
+                    'payment_2', 'payment_2_datetime',
+                    'payment_3', 'payment_3_datetime']
+    readonly_fields = ['manager_updated_at', 'vyavasthapak_updated_at', 'trustee_updated_at',
+                       'finance_house_head_updated_at', 'Ambareeshbhai_updated_at', 'Cashier_updated_at',
+                       'payment_1_datetime', 'payment_2_datetime', 'payment_3_datetime', 'created_at']
+
+
+class DakshinBharatSettings(admin.ModelAdmin):
+    list_display = ['description', 'manager', 'vyavasthapak', 'trustee', 'finance_house_head', 'Ambareeshbhai',
+                    'Cashier', 'quotation_file_1', 'quotation_file_2', 'quotation_file_3', 'bill_file',
+                    'payment_1', 'payment_1_datetime',
+                    'payment_2', 'payment_2_datetime',
+                    'payment_3', 'payment_3_datetime']
+    readonly_fields = ['manager_updated_at', 'vyavasthapak_updated_at', 'trustee_updated_at',
+                       'finance_house_head_updated_at', 'Ambareeshbhai_updated_at', 'Cashier_updated_at',
+                       'payment_1_datetime', 'payment_2_datetime', 'payment_3_datetime', 'created_at']
+
+
+admin.site.register(DandiSamarpanAashram, DandiSettings)
+admin.site.register(KutchSamarpanAashram, KutchSettings)
+admin.site.register(SaurastraSamarpanAashram, SaurastraSettings)
+admin.site.register(GujaratSamarpanAashram, GujaratSettings)
+admin.site.register(GoaSamarpanAashram, GoaSettings)
+admin.site.register(MadhyaBharatSamarpanAashram, MadhyaBharatSettings)
+admin.site.register(RajasthanSamarpanAashram, RajasthanSettings)
+admin.site.register(DakshinBharatShreeShivkrupanandSwamiMath, DakshinBharatSettings)
--- AashramFinance/models.py
+++ AashramFinance/models.py
@@ -2,136 +2,256 @@ from django.db import models
 
 
 class DandiSamarpanAashram(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    manager = models.BooleanField(max_length=100)
-    vyavasthapak = models.BooleanField(max_length=100)
-    trustee = models.BooleanField(max_length=100)
-    Amareeshbhai = models.BooleanField(max_length=100)
-    Cashier = models.BooleanField(max_length=100)
-    bill_file = models.FileField(upload_to='bill_files/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = models.manager
+    class Meta:
+        verbose_name = 'Dandi Samarpan Aashram'
+        verbose_name_plural = 'Dandi Samarpan Aashram'
 
-    def __str__(self):
-        return self.title
+    description = models.TextField()
+    manager = models.BooleanField()
+    manager_updated_at = models.DateTimeField(auto_now=True)
+    vyavasthapak = models.BooleanField()
+    vyavasthapak_updated_at = models.DateTimeField(auto_now=True)
+    trustee = models.BooleanField()
+    trustee_updated_at = models.DateTimeField(auto_now=True)
+    finance_house_head = models.BooleanField()
+    finance_house_head_updated_at = models.DateTimeField(auto_now=True)
+    Ambareeshbhai = models.BooleanField()
+    Ambareeshbhai_updated_at = models.DateTimeField(auto_now=True)
+    Cashier = models.BooleanField()
+    Cashier_updated_at = models.DateTimeField(auto_now=True)
+    quotation_file_1 = models.FileField(upload_to='quotation_files_1/')
+    quotation_file_2 = models.FileField(upload_to='quotation_files_2/')
+    quotation_file_3 = models.FileField(upload_to='quotation_files_3/')
+    bill_file = models.FileField(upload_to='bill_files/')
+    payment_1 = models.FloatField()
+    payment_1_datetime = models.DateTimeField(auto_now=True)
+    payment_2 = models.FloatField()
+    payment_2_datetime = models.DateTimeField(auto_now=True)
+    payment_3 = models.FloatField()
+    payment_3_datetime = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = models.manager
 
 
 class KutchSamarpanAashram(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    manager = models.BooleanField(max_length=100)
-    vyavasthapak = models.BooleanField(max_length=100)
-    trustee = models.BooleanField(max_length=100)
-    Amareeshbhai = models.BooleanField(max_length=100)
-    Cashier = models.BooleanField(max_length=100)
-    bill_file = models.FileField(upload_to='bill_files/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = models.manager
+    class Meta:
+        verbose_name = 'Kutch Samarpan Aashram'
+        verbose_name_plural = 'Kutch Samarpan Aashram'
 
-    def __str__(self):
-        return self.title
+    description = models.TextField()
+    manager = models.BooleanField()
+    manager_updated_at = models.DateTimeField(auto_now=True)
+    vyavasthapak = models.BooleanField()
+    vyavasthapak_updated_at = models.DateTimeField(auto_now=True)
+    trustee = models.BooleanField()
+    trustee_updated_at = models.DateTimeField(auto_now=True)
+    finance_house_head = models.BooleanField()
+    finance_house_head_updated_at = models.DateTimeField(auto_now=True)
+    Ambareeshbhai = models.BooleanField()
+    Ambareeshbhai_updated_at = models.DateTimeField(auto_now=True)
+    Cashier = models.BooleanField()
+    Cashier_updated_at = models.DateTimeField(auto_now=True)
+    quotation_file_1 = models.FileField(upload_to='quotation_files_1/')
+    quotation_file_2 = models.FileField(upload_to='quotation_files_2/')
+    quotation_file_3 = models.FileField(upload_to='quotation_files_3/')
+    bill_file = models.FileField(upload_to='bill_files/')
+    payment_1 = models.FloatField()
+    payment_1_datetime = models.DateTimeField(auto_now=True)
+    payment_2 = models.FloatField()
+    payment_2_datetime = models.DateTimeField(auto_now=True)
+    payment_3 = models.FloatField()
+    payment_3_datetime = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = models.manager
 
 
 class SaurastraSamarpanAashram(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    manager = models.BooleanField(max_length=100)
-    vyavasthapak = models.BooleanField(max_length=100)
-    trustee = models.BooleanField(max_length=100)
-    Amareeshbhai = models.BooleanField(max_length=100)
-    Cashier = models.BooleanField(max_length=100)
-    bill_file = models.FileField(upload_to='bill_files/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = models.manager
+    class Meta:
+        verbose_name = 'Saurastra Samarpan Aashram'
+        verbose_name_plural = 'Saurastra Samarpan Aashram'
 
-    def __str__(self):
-        return self.title
+    description = models.TextField()
+    manager = models.BooleanField()
+    manager_updated_at = models.DateTimeField(auto_now=True)
+    vyavasthapak = models.BooleanField()
+    vyavasthapak_updated_at = models.DateTimeField(auto_now=True)
+    trustee = models.BooleanField()
+    trustee_updated_at = models.DateTimeField(auto_now=True)
+    finance_house_head = models.BooleanField()
+    finance_house_head_updated_at = models.DateTimeField(auto_now=True)
+    Ambareeshbhai = models.BooleanField()
+    Ambareeshbhai_updated_at = models.DateTimeField(auto_now=True)
+    Cashier = models.BooleanField()
+    Cashier_updated_at = models.DateTimeField(auto_now=True)
+    quotation_file_1 = models.FileField(upload_to='quotation_files_1/')
+    quotation_file_2 = models.FileField(upload_to='quotation_files_2/')
+    quotation_file_3 = models.FileField(upload_to='quotation_files_3/')
+    bill_file = models.FileField(upload_to='bill_files/')
+    payment_1 = models.FloatField()
+    payment_1_datetime = models.DateTimeField(auto_now=True)
+    payment_2 = models.FloatField()
+    payment_2_datetime = models.DateTimeField(auto_now=True)
+    payment_3 = models.FloatField()
+    payment_3_datetime = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = models.manager
 
 
 class GujaratSamarpanAashram(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    manager = models.BooleanField(max_length=100)
-    vyavasthapak = models.BooleanField(max_length=100)
-    trustee = models.BooleanField(max_length=100)
-    Amareeshbhai = models.BooleanField(max_length=100)
-    Cashier = models.BooleanField(max_length=100)
-    bill_file = models.FileField(upload_to='bill_files/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = models.manager
+    class Meta:
+        verbose_name = 'Gujarat Samarpan Aashram'
+        verbose_name_plural = 'Gujarat Samarpan Aashram'
 
-    def __str__(self):
-        return self.title
+    description = models.TextField()
+    manager = models.BooleanField()
+    manager_updated_at = models.DateTimeField(auto_now=True)
+    vyavasthapak = models.BooleanField()
+    vyavasthapak_updated_at = models.DateTimeField(auto_now=True)
+    trustee = models.BooleanField()
+    trustee_updated_at = models.DateTimeField(auto_now=True)
+    finance_house_head = models.BooleanField()
+    finance_house_head_updated_at = models.DateTimeField(auto_now=True)
+    Ambareeshbhai = models.BooleanField()
+    Ambareeshbhai_updated_at = models.DateTimeField(auto_now=True)
+    Cashier = models.BooleanField()
+    Cashier_updated_at = models.DateTimeField(auto_now=True)
+    quotation_file_1 = models.FileField(upload_to='quotation_files_1/')
+    quotation_file_2 = models.FileField(upload_to='quotation_files_2/')
+    quotation_file_3 = models.FileField(upload_to='quotation_files_3/')
+    bill_file = models.FileField(upload_to='bill_files/')
+    payment_1 = models.FloatField()
+    payment_1_datetime = models.DateTimeField(auto_now=True)
+    payment_2 = models.FloatField()
+    payment_2_datetime = models.DateTimeField(auto_now=True)
+    payment_3 = models.FloatField()
+    payment_3_datetime = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = models.manager
 
 
 class RajasthanSamarpanAashram(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    manager = models.BooleanField(max_length=100)
-    vyavasthapak = models.BooleanField(max_length=100)
-    trustee = models.BooleanField(max_length=100)
-    Amareeshbhai = models.BooleanField(max_length=100)
-    Cashier = models.BooleanField(max_length=100)
-    bill_file = models.FileField(upload_to='bill_files/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = models.manager
+    class Meta:
+        verbose_name = 'Rajasthan Samarpan Aashram'
+        verbose_name_plural = 'Rajasthan Samarpan Aashram'
 
-    def __str__(self):
-        return self.title
+    description = models.TextField()
+    manager = models.BooleanField()
+    manager_updated_at = models.DateTimeField(auto_now=True)
+    vyavasthapak = models.BooleanField()
+    vyavasthapak_updated_at = models.DateTimeField(auto_now=True)
+    trustee = models.BooleanField()
+    trustee_updated_at = models.DateTimeField(auto_now=True)
+    finance_house_head = models.BooleanField()
+    finance_house_head_updated_at = models.DateTimeField(auto_now=True)
+    Ambareeshbhai = models.BooleanField()
+    Ambareeshbhai_updated_at = models.DateTimeField(auto_now=True)
+    Cashier = models.BooleanField()
+    Cashier_updated_at = models.DateTimeField(auto_now=True)
+    quotation_file_1 = models.FileField(upload_to='quotation_files_1/')
+    quotation_file_2 = models.FileField(upload_to='quotation_files_2/')
+    quotation_file_3 = models.FileField(upload_to='quotation_files_3/')
+    bill_file = models.FileField(upload_to='bill_files/')
+    payment_1 = models.FloatField()
+    payment_1_datetime = models.DateTimeField(auto_now=True)
+    payment_2 = models.FloatField()
+    payment_2_datetime = models.DateTimeField(auto_now=True)
+    payment_3 = models.FloatField()
+    payment_3_datetime = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = models.manager
 
 
 class GoaSamarpanAashram(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    manager = models.BooleanField(max_length=100)
-    vyavasthapak = models.BooleanField(max_length=100)
-    trustee = models.BooleanField(max_length=100)
-    Amareeshbhai = models.BooleanField(max_length=100)
-    Cashier = models.BooleanField(max_length=100)
-    bill_file = models.FileField(upload_to='bill_files/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = models.manager
+    class Meta:
+        verbose_name = 'Goa Samarpan Aashram'
+        verbose_name_plural = 'Goa Samarpan Aashram'
 
-    def __str__(self):
-        return self.title
+    description = models.TextField()
+    manager = models.BooleanField()
+    manager_updated_at = models.DateTimeField(auto_now=True)
+    vyavasthapak = models.BooleanField()
+    vyavasthapak_updated_at = models.DateTimeField(auto_now=True)
+    trustee = models.BooleanField()
+    trustee_updated_at = models.DateTimeField(auto_now=True)
+    finance_house_head = models.BooleanField()
+    finance_house_head_updated_at = models.DateTimeField(auto_now=True)
+    Ambareeshbhai = models.BooleanField()
+    Ambareeshbhai_updated_at = models.DateTimeField(auto_now=True)
+    Cashier = models.BooleanField()
+    Cashier_updated_at = models.DateTimeField(auto_now=True)
+    quotation_file_1 = models.FileField(upload_to='quotation_files_1/')
+    quotation_file_2 = models.FileField(upload_to='quotation_files_2/')
+    quotation_file_3 = models.FileField(upload_to='quotation_files_3/')
+    bill_file = models.FileField(upload_to='bill_files/')
+    payment_1 = models.FloatField()
+    payment_1_datetime = models.DateTimeField(auto_now=True)
+    payment_2 = models.FloatField()
+    payment_2_datetime = models.DateTimeField(auto_now=True)
+    payment_3 = models.FloatField()
+    payment_3_datetime = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = models.manager
 
 
 class MadhyaBharatSamarpanAashram(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    manager = models.BooleanField(max_length=100)
-    vyavasthapak = models.BooleanField(max_length=100)
-    trustee = models.BooleanField(max_length=100)
-    Amareeshbhai = models.BooleanField(max_length=100)
-    Cashier = models.BooleanField(max_length=100)
-    bill_file = models.FileField(upload_to='bill_files/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = models.manager
+    class Meta:
+        verbose_name = 'Madhya Bharat Samarpan Aashram'
+        verbose_name_plural = 'Madhya Bharat Samarpan Aashram'
 
-    def __str__(self):
-        return self.title
+    description = models.TextField()
+    manager = models.BooleanField()
+    manager_updated_at = models.DateTimeField(auto_now=True)
+    vyavasthapak = models.BooleanField()
+    vyavasthapak_updated_at = models.DateTimeField(auto_now=True)
+    trustee = models.BooleanField()
+    trustee_updated_at = models.DateTimeField(auto_now=True)
+    finance_house_head = models.BooleanField()
+    finance_house_head_updated_at = models.DateTimeField(auto_now=True)
+    Ambareeshbhai = models.BooleanField()
+    Ambareeshbhai_updated_at = models.DateTimeField(auto_now=True)
+    Cashier = models.BooleanField()
+    Cashier_updated_at = models.DateTimeField(auto_now=True)
+    quotation_file_1 = models.FileField(upload_to='quotation_files_1/')
+    quotation_file_2 = models.FileField(upload_to='quotation_files_2/')
+    quotation_file_3 = models.FileField(upload_to='quotation_files_3/')
+    bill_file = models.FileField(upload_to='bill_files/')
+    payment_1 = models.FloatField()
+    payment_1_datetime = models.DateTimeField(auto_now=True)
+    payment_2 = models.FloatField()
+    payment_2_datetime = models.DateTimeField(auto_now=True)
+    payment_3 = models.FloatField()
+    payment_3_datetime = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = models.manager
 
 
 class DakshinBharatShreeShivkrupanandSwamiMath(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    manager = models.BooleanField(max_length=100)
-    vyavasthapak = models.BooleanField(max_length=100)
-    trustee = models.BooleanField(max_length=100)
-    Amareeshbhai = models.BooleanField(max_length=100)
-    Cashier = models.BooleanField(max_length=100)
-    bill_file = models.FileField(upload_to='bill_files/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = models.manager
+    class Meta:
+        verbose_name = 'Dakshin Bharat Shree Shivkrupanand Swami Math'
+        verbose_name_plural = 'Dakshin Bharat Shree Shivkrupanand Swami Math'
 
-    def __str__(self):
-        return self.title
+    description = models.TextField()
+    manager = models.BooleanField()
+    manager_updated_at = models.DateTimeField(auto_now=True)
+    vyavasthapak = models.BooleanField()
+    vyavasthapak_updated_at = models.DateTimeField(auto_now=True)
+    trustee = models.BooleanField()
+    trustee_updated_at = models.DateTimeField(auto_now=True)
+    finance_house_head = models.BooleanField()
+    finance_house_head_updated_at = models.DateTimeField(auto_now=True)
+    Ambareeshbhai = models.BooleanField()
+    Ambareeshbhai_updated_at = models.DateTimeField(auto_now=True)
+    Cashier = models.BooleanField()
+    Cashier_updated_at = models.DateTimeField(auto_now=True)
+    quotation_file_1 = models.FileField(upload_to='quotation_files_1/')
+    quotation_file_2 = models.FileField(upload_to='quotation_files_2/')
+    quotation_file_3 = models.FileField(upload_to='quotation_files_3/')
+    bill_file = models.FileField(upload_to='bill_files/')
+    payment_1 = models.FloatField()
+    payment_1_datetime = models.DateTimeField(auto_now=True)
+    payment_2 = models.FloatField()
+    payment_2_datetime = models.DateTimeField(auto_now=True)
+    payment_3 = models.FloatField()
+    payment_3_datetime = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = models.manager
